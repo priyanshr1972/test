dbae39029b35c9251c2ace16a85d0b0266afa8dc
priyanshr1972 <priyanshr1972@gmail.com>
2024-01-01 22:22:59 +0530

    cm

---
+++ scripts/refurbishedInner2.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+  function myFunctionRefurbished(btn) {
+    var container = btn.parentElement.parentElement; 
+
+    var dots = container.querySelector("#dots");
+    var moreText = container.querySelector("#more");
+    var btnText = btn;
+
+    if (dots.style.display === "none" || dots.style.display === "") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
